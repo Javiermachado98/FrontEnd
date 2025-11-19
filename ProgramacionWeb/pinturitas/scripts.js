a2b834bf37871494831fbe9ee059f0fabe4240e8
Javiermachado98 <javier-0198@hotmail.com>
2025-11-19 14:13:17 +0100

    cambios

--- ProgramacionWeb/pinturitas/scripts.js
+++ ProgramacionWeb/pinturitas/scripts.js
@@ -1,13 +1,36 @@
 const nFilas = 20 // importante ya que es el limite
 const nCol = 20
 let orderStatus = false
+let colorPintador;
 
-
-let pinturitas = document.getElementsByClassName('pinturitas')[0]
+// aqui podemos traer los datos en base a un arreglo de colores
 let colores = ["yellow", "blue", "green", "red", "grey", "white"]
+for (let i = 0; i < colores.length; i++){
+    console.log("color seleccionado:", colores[i])
+    let element = document.getElementById(colores[i])
+    element.addEventListener('click', selectColor)
+}
+let pinturitas = document.getElementsByClassName('pinturitas')[0]
 
+
+/*
+//Aqui podemos traer los datos por colores y su clase
+let coloresHTML = document.getElementsByClassName('item')
+for (let i = 0; i < coloresHTML.length; i++){
+    coloresHTML[i].addEventListener('click', selectColor)
+}*/
+
+//Lo podemos llamar por id
+function selectColor() {
+    console.log(event.target.id)
+    colorPintador = event.target.id
+}
+/*
+Aqui podemos llamar 
+function selectColor() {
+    console.log(event.target.classList[1])
+}*/
 // For sobre el htmlcollection getellñalskdlajsd('item')
-
 
 function startPrint(){
     orderStatus = !orderStatus
@@ -15,7 +38,9 @@ function startPrint(){
 
 function paintColor(){
     if (orderStatus) {
-        console.log("pintar colores")
+        // event.target me recupera el td que ha llamado al evento
+        event.target.style.backgroundColor = colorPintador
+        //event.target.classList.add(colorPintador), esta no por que no sobreescribe
     }
 }
 
@@ -35,7 +60,7 @@ for (i = 0; i < nFilas; i++){
     for(j = 0; j < nCol; j++){
         let td = tr.insertCell()
         td.addEventListener('click', startPrint)
-        td.addEventListener('click', paintColor)
+        td.addEventListener('mousemove', paintColor)
     }
 }
 
