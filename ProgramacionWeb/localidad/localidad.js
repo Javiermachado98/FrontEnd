f3539c8aba3a28040644f572fb0aa988c577d916
Javiermachado98 <javier-0198@hotmail.com>
2025-11-20 11:11:14 +0100

    cambios

---
+++ ProgramacionWeb/localidad/localidad.js
@@ -0,0 +1,8 @@
+let enviar = document.getElementById('enviar')
+enviar.addEventListener('click', arrancar)
+
+function arrancar(){
+    let pueblo = document.getElementById('localidad').value
+    request = new XMLHttpRequest()
+    request
+}
---
+++ ProgramacionWeb/localidad/localidad.php
@@ -0,0 +1,7 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+$ciudades = array("Monzon", "Binefar", "Tamarite", "Barcelona", "Madrid");
+
+$ciudades = $_REQUEST["nombre"];
+sleep(5);
+?>
